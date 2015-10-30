60bdc9f6b49fe95eef82ed87f2931adb8f1b71ee
terrylowery <terry@lowerytech.net>
2015-10-30 17:29:47 -0500

    Exercise_33

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
   def index
   end
+  def landing_page
+    if Product.first
+    @featured_product = Product.first
+    else
+      @featured_product = nil
+    end
+  end
+
 end
