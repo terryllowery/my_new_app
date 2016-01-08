e43a4defa85df40ebc3f2e0f288c3fce1afd2752
terrylowery <terry@lowerytech.net>
2016-01-07 20:29:23 -0600

    exercise 5.1

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
   def landing_page
       @products = Product.limit(3)
   end
+  def home
+    redirect_to(static_pages_index_path)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :products
+
+	resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
-  get 'static_pages/about'
+	get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
+
+  get 'static_pages/home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
